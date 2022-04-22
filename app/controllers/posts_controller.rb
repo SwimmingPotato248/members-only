644bdeec58c43a9b2c0bc655b7f86b38c6df131c
Swimming Potato <ptphong200i@gmail.com>
2022-04-22 09:01:38 +0700

    Add index page to show the posts and allow user to create new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,31 @@
 class PostsController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.post.build
+  end
+
+  def create
+    @post = current_user.post.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to posts_path, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:user_id, :post_content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {  }
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :new, :create]
   # Defines the root path route ("/")
-  root "user#index"
+  root "posts#index"
 end
